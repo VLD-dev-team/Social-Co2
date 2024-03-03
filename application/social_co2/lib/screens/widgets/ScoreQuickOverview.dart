59940b293d4af7453f33d1b7adf4df0d577066c6
Vrock691 <vrock691@gmail.com>
2024-03-03 20:11:19 +0100

    feat: avancé du widget QuickScoreOverview - Ajout du score et début de l'échelle de score

--- application/social_co2/lib/screens/widgets/ScoreQuickOverview.dart
+++ application/social_co2/lib/screens/widgets/ScoreQuickOverview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_co2/providers/IndexProvider.dart';
 import 'package:social_co2/styles/CardStyles.dart';
 
 class ScoreQuickOverview extends StatefulWidget {
@@ -10,90 +12,156 @@ class _ScoreQuickOverviewState extends State<ScoreQuickOverview> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: primaryCardColor,
+      decoration: primaryCard,
       child: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
+        child: Table(children: [
+          TableRow(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  decoration: secondaryCardInnerShadow,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Card(
+                            margin: EdgeInsets.all(10.0),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              child: Text(
+                                "Aujourdhui",
+                                style: TextStyle(fontSize: 25),
+                              ),
+                            )),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                          color: primaryCardColor,
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                                "Lorem IPSUjejzfzefnjzfnjdfkjdfnkjdfgnkjdfgnkdjfgnkjdfgkdjfngkjdfngkjdfngkjdfngkdfjngkdfjgnkdfjgnkdjfgnkdfjgkdfjgkdjfgnkdfsjgksdjfgnkdfjgndfjkgndkjfs"),
+                          ),
+                        ),
+                      ]),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: secondaryCardInnerShadow,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Card(
+                            margin: EdgeInsets.all(10.0),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              child: Text(
+                                "Hier",
+                                style: TextStyle(fontSize: 25),
+                              ),
+                            )),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                          color: primaryCardColor,
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                                "Lorem IPSUjejzfzefnjzfnjdfkjdfnkjdfgnkjdfgnkdjfgnkjdfgkdjfngkjdfngkjdfngkjdfngkdfjngkdfjgnkdfjgnkdjfgnkdfjgkdfjgkdjfgnkdfsjgksdjfgnkdfjgndfjkgndkjfs"),
+                          ),
+                        ),
+                      ]),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+            TableCell(
+                verticalAlignment: TableCellVerticalAlignment.fill,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, bottom: 5),
+                  child: Container(
+                    margin: EdgeInsets.zero,
                     decoration: secondaryCardInnerShadow,
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Card(
-                              margin: EdgeInsets.all(10.0),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 5, horizontal: 10),
-                                child: Text(
-                                  "Aujourdhui",
-                                  style: TextStyle(fontSize: 25),
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          decoration: primaryCard,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                const Card(
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 10),
+                                    child: Text(
+                                      "Votre score",
+                                      style: TextStyle(fontSize: 25),
+                                    ),
+                                  ),
                                 ),
-                              )),
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                            color: primaryCardColor,
-                            child: Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: Text(
-                                  "Lorem IPSUjejzfzefnjzfnjdfkjdfnkjdfgnkjdfgnkdjfgnkjdfgkdjfngkjdfngkjdfngkjdfngkdfjngkdfjgnkdfjgnkdjfgnkdfjgkdfjgkdjfgnkdfsjgksdjfgnkdfjgndfjkgndkjfs"),
+                                Card(
+                                  color: secondaryCardColor,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 10),
+                                    child: Text(
+                                      '- ${Provider.of<IndexProvider>(listen: false, context).selectedIndex.toString()} SCO -',
+                                      style: const TextStyle(fontSize: 25),
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                        ]),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    decoration: secondaryCardInnerShadow,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Card(
-                              margin: EdgeInsets.all(10.0),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 5, horizontal: 10),
-                                child: Text(
-                                  "Hier",
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                              )),
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                            color: primaryCardColor,
-                            child: Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: Text(
-                                  "Lorem IPSUjejzfzefnjzfnjdfkjdfnkjdfgnkjdfgnkdjfgnkjdfgkdjfngkjdfngkjdfngkjdfngkdfjngkdfjgnkdfjgnkdjfgnkdfjgkdfjgkdjfgnkdfsjgksdjfgnkdfjgndfjkgndkjfs"),
-                            ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: primaryCard,
+                          height: 150,
+                          child: GridView.count(
+                            crossAxisCount: 6,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [Text("data")],
+                              ),
+                            ],
                           ),
-                        ]),
+                        )
+                      ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: EdgeInsets.zero,
-                decoration: secondaryCardInnerShadow,
-                child: Text('dada'),
-              ),
-            )
-          ],
-        ),
+                ))
+          ]),
+        ]),
       ),
     );
   }
--- application/social_co2/lib/styles/CardStyles.dart
+++ application/social_co2/lib/styles/CardStyles.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 const Color primaryCardColor = Color.fromRGBO(249, 239, 219, 1);
 const Color secondaryCardColor = Color.fromRGBO(235, 217, 180, 1);
+const Color tertiaryCardColor = Color.fromRGBO(176, 162, 133, 1);
 const Color shadowColor = Color.fromARGB(64, 0, 0, 0);
+
+const BoxDecoration primaryCard = BoxDecoration(
+  color: primaryCardColor,
+  /* boxShadow: [
+    BoxShadow(
+      color: shadowColor,
+    ),
+    BoxShadow(
+        color: secondaryCardColor,
+        //blurRadius: 4,
+        spreadRadius: 0,
+        offset: Offset(0, 4)),
+  ], */
+  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+);
 
 const BoxDecoration secondaryCardInnerShadow = BoxDecoration(
     color: secondaryCardColor,

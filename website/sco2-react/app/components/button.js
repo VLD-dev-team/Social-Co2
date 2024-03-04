51518808b95a228a3f325edde9f414b024e1d2e4
Camille <camille.dehen@gmail.com>
2024-03-04 11:01:44 +0100

    feat: refactorisation du code + ajout d'une image de fond + avancement de la page d'accueil

---
+++ website/sco2-react/app/components/button.js
@@ -0,0 +1,7 @@
+const Button = ({onClick, children}) =>{
+    return (
+        <button onClick={onClick}>
+            {children}
+        </button>
+    );
+};
--- website/sco2-react/tailwind.config.ts
+++ website/sco2-react/tailwind.config.ts
@@ -7,11 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    /*fontFamily: {
+      'sans':['readex pro'],
+    },*/
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50% var(--tw-gradient-stops))",
       },
     },
   },

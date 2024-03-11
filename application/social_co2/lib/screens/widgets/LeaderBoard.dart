6e2b049d446d419bdbd36861d12bd98c39307155
Gookd <jerem140802@gmail.com>
2024-03-11 16:44:06 +0100

    feat : avancée du widget leaderboard

--- application/social_co2/lib/screens/widgets/LeaderBoard.dart
+++ application/social_co2/lib/screens/widgets/LeaderBoard.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:social_co2/styles/CardStyles.dart';
 
 class LeaderBoard extends StatelessWidget {
   const LeaderBoard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Text('LeaderBoard');
-  }
+Widget build(BuildContext context) {
+  return TableCell(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          decoration: secondaryCardInnerShadow,
+          child: Card(
+            margin: EdgeInsets.all(10.0),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: Center(
+                child: Container( 
+                  child: Text(
+                    "Leaderboard",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }

8b2f146606f29f29ea214da75ff3df46d31384f9
Vrock691 <vrock691@gmail.com>
2024-03-11 18:15:55 +0100

    fix: Suppression de TableCell dans LeaderBoard.dart

--- application/social_co2/lib/screens/widgets/LeaderBoard.dart
+++ application/social_co2/lib/screens/widgets/LeaderBoard.dart
@@ -5,30 +5,23 @@ class LeaderBoard extends StatelessWidget {
   const LeaderBoard({super.key});
 
   @override
-Widget build(BuildContext context) {
-  return TableCell(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          decoration: secondaryCardInnerShadow,
-          child: Card(
-            margin: EdgeInsets.all(10.0),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              child: Center(
-                child: Container( 
-                  child: Text(
-                    "Leaderboard",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: secondaryCardInnerShadow,
+      child: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Center(
+            child: Container(
+              child: Text(
+                "Leaderboard",
+                style: TextStyle(fontSize: 25),
               ),
             ),
           ),
         ),
-      ],
-    ),
-  );
-}
+      ),
+    );
+  }
 }

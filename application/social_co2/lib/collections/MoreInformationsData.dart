4c5f56aa9c12f48b2f18b8b89dab2f6c0014b7bc
Vrock691 <vrock691@gmail.com>
2024-03-18 13:54:24 +0100

    feat: form activity more infos terminé visuellement, commencement des requettes serveur

--- application/social_co2/lib/collections/MoreInformationsData.dart
+++ application/social_co2/lib/collections/MoreInformationsData.dart
@@ -1,1 +1,17 @@
 enum HeatingModes { gaz, pellet, bois, electrique, fioul }
+
+const Map<HeatingModes, String> heatingModesNames = {
+  HeatingModes.bois: "Poêle à bois",
+  HeatingModes.electrique: "Éléctrique",
+  HeatingModes.fioul: "Fioul",
+  HeatingModes.gaz: "Gaz",
+  HeatingModes.pellet: "Pellet"
+};
+
+enum CarSizes { small, mid, big }
+
+const Map<CarSizes, String> carSizesNames = {
+  CarSizes.small: "Petite voiture",
+  CarSizes.mid: "Voiture moyenne",
+  CarSizes.big: "Véhicule imposant"
+};
--- application/social_co2/lib/screens/widgets/CardMoreInformations.dart
+++ application/social_co2/lib/screens/widgets/CardMoreInformations.dart
@@ -12,7 +12,10 @@ class CardMoreInformations extends StatefulWidget {
 }
 
 class _CardMoreInformations extends State<CardMoreInformations> {
-  HeatingModes? _heatingModes = HeatingModes.gaz;
+  HeatingModes? _selectedHeatingMode = HeatingModes.gaz;
+  CarSizes? _selectedCarSize = CarSizes.mid;
+  bool isCarHybrid = false;
+  bool saved = true;
 
   @override
   Widget build(BuildContext context) {
@@ -48,19 +51,29 @@ class _CardMoreInformations extends State<CardMoreInformations> {
                   Container(
                       margin: const EdgeInsets.symmetric(
                           vertical: 5, horizontal: 10),
-                      decoration: primaryCard,
-                      child: const ListTile(
-                        title: Text("Maison"),
+                      child: ListTile(
+                        title: const Text("Maison"),
+                        subtitle: Wrap(
+                          children: const [
+                            Text(
+                                "Informations sur votre lieu de vie principal.")
+                          ],
+                        ),
                       )),
                   Container(
                       margin: const EdgeInsets.symmetric(
                           vertical: 5, horizontal: 10),
                       decoration: primaryCard,
-                      child: const ListTile(
-                          leading: Text("Nombre de m² : "),
+                      child: ListTile(
+                          title: const Text("Nombre de m² : "),
                           trailing: SizedBox(
                               width: 60,
                               child: TextField(
+                                  onChanged: (value) {
+                                    setState(() {
+                                      saved = false;
+                                    });
+                                  },
                                   textAlign: TextAlign.center,
                                   keyboardType: TextInputType.number)))),
                   Container(
@@ -68,37 +81,203 @@ class _CardMoreInformations extends State<CardMoreInformations> {
                           vertical: 5, horizontal: 10),
                       decoration: primaryCard,
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           const ListTile(
-                            leading: Text("Type de chauffage"),
+                            title: Text("Type de chauffage"),
                           ),
-                          Wrap(
-                            children: [
-                              SizedBox(
-                                child: GestureDetector(
-                                  child: Wrap(
-                                    children: [
-                                      Radio<HeatingModes>(
-                                          value: HeatingModes.bois,
-                                          groupValue: _heatingModes,
-                                          onChanged: (HeatingModes? value) {
-                                            setState(() {
-                                              _heatingModes = value;
-                                            });
-                                          }),
-                                      const Text("Bois")
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Wrap(
+                              spacing: 10.0,
+                              runSpacing: 10.0,
+                              children: [
+                                heatModeRadioButton(HeatingModes.bois),
+                                heatModeRadioButton(HeatingModes.electrique),
+                                heatModeRadioButton(HeatingModes.fioul),
+                                heatModeRadioButton(HeatingModes.gaz),
+                                heatModeRadioButton(HeatingModes.pellet),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
                           )
                         ],
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      decoration: primaryCard,
+                      child: ListTile(
+                          title: const Text("Nombre de chauffage : "),
+                          trailing: SizedBox(
+                              width: 60,
+                              child: TextField(
+                                  onChanged: (value) {
+                                    setState(() {
+                                      saved = false;
+                                    });
+                                  },
+                                  textAlign: TextAlign.center,
+                                  keyboardType: TextInputType.number)))),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      decoration: primaryCard,
+                      child: ListTile(
+                          title: Text(
+                              "Date de construction du batiments (ou date de rénovation): "),
+                          trailing: SizedBox(
+                              width: 60,
+                              child: TextField(
+                                  onChanged: (value) {
+                                    setState(() {
+                                      saved = false;
+                                    });
+                                  },
+                                  textAlign: TextAlign.center,
+                                  keyboardType: TextInputType.number)))),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: ListTile(
+                        title: const Text("Voiture personnelle"),
+                        subtitle: Wrap(
+                          children: const [
+                            Text(
+                                "Si votre voiture principale est 100% éléctrique, cette section est facultative")
+                          ],
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      decoration: primaryCard,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const ListTile(
+                            title: Text("Taille du vehicule"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Wrap(
+                              spacing: 10.0,
+                              runSpacing: 10.0,
+                              children: [
+                                CarSizeRadioButton(CarSizes.small),
+                                CarSizeRadioButton(CarSizes.mid),
+                                CarSizeRadioButton(CarSizes.big),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      decoration: primaryCard,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            saved = false;
+                            isCarHybrid = !isCarHybrid;
+                          });
+                        },
+                        child: ListTile(
+                            title: const Text("Hybride : "),
+                            trailing: SizedBox(
+                                width: 60,
+                                child: Switch(
+                                    value: isCarHybrid,
+                                    onChanged: (bool value) {
+                                      setState(() {
+                                        saved = false;
+                                        isCarHybrid = value;
+                                      });
+                                    }))),
                       )),
                 ],
               ),
             ),
-          )
+          ),
+          if (!saved)
+            FloatingActionButton.extended(
+              icon: const Icon(Icons.save),
+              label: const Text("Sauvegarder"),
+              onPressed: () {
+                setState(() {
+                  saved = true;
+                });
+              },
+            ),
+          if (!saved)
+            const SizedBox(
+              height: 15,
+            )
         ]));
+  }
+
+  SizedBox heatModeRadioButton(HeatingModes mode) {
+    return SizedBox(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _selectedHeatingMode = mode;
+            saved = false;
+          });
+        },
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            Radio<HeatingModes>(
+                value: mode,
+                groupValue: _selectedHeatingMode,
+                onChanged: (HeatingModes? value) {
+                  setState(() {
+                    saved = false;
+                    _selectedHeatingMode = value;
+                  });
+                }),
+            Text('${heatingModesNames[mode]}')
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox CarSizeRadioButton(CarSizes size) {
+    return SizedBox(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            saved = false;
+            _selectedCarSize = size;
+          });
+        },
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            Radio<CarSizes>(
+                value: size,
+                groupValue: _selectedCarSize,
+                onChanged: (CarSizes? value) {
+                  setState(() {
+                    saved = false;
+                    _selectedCarSize = value;
+                  });
+                }),
+            Text('${carSizesNames[size]}')
+          ],
+        ),
+      ),
+    );
   }
 }

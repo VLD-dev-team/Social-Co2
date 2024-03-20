aed3824188c2df31ce79c787213b50cf4820d84c
Vrock691 <vrock691@gmail.com>
2024-03-20 17:04:12 +0100

    fix: Constante non valide

--- application/social_co2/lib/screens/homeScreen/LeaderBoardScreen.dart
+++ application/social_co2/lib/screens/homeScreen/LeaderBoardScreen.dart
@@ -6,65 +6,66 @@ import 'package:social_co2/utils/responsiveHandler.dart';
 
 class LeaderBoardScreeen extends StatelessWidget {
   const LeaderBoardScreeen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     ResponsiveFormats responsiveFormat = whichResponsiveFormat(context);
     int drawerWidth = getDrawerWidth(context);
 
-  return Container(
-  decoration: homeScreenBackground,
-  height: double.infinity,
-  width: MediaQuery.of(context).size.width - drawerWidth,
-  child: Padding(
-    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-    child: Container(
-      decoration: primaryCard,
-      child:  Column(
-        crossAxisAlignment: CrossAxisAlignment.center, 
-        children: [
-          const Card(
-            child: Center(
-              child: Text(
-                "Leaderboard",
-                style: TextStyle(fontSize: 25),
+    return Container(
+      decoration: homeScreenBackground,
+      height: double.infinity,
+      width: MediaQuery.of(context).size.width - drawerWidth,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: Container(
+          decoration: primaryCard,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Card(
+                child: Center(
+                  child: Text(
+                    "Leaderboard",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            decoration: secondaryCardInnerShadow,
-            child: const Row(
-               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Card(
-                        child: Text("Mondial", style: TextStyle(fontSize: 25)),
+              Container(
+                decoration: secondaryCardInnerShadow,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Card(
+                            child:
+                                Text("Mondial", style: TextStyle(fontSize: 25)),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    VerticalDivider(
+                      thickness: 10,
+                      color: Colors.white,
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Card(
+                            child: Text("Amis", style: TextStyle(fontSize: 25)),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                VerticalDivider(
-                  thickness: 10,
-                  color: Colors.white,
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Card(
-                        child: Text("Amis", style: TextStyle(fontSize: 25)),),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  ),
-);
-    
-}
+    );
+  }
 }

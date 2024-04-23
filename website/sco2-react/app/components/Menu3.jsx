84d40f9709af5d3b4380126081d309e5a13f7bf4
Vrock691 <vrock691@gmail.com>
2024-04-23 11:21:58 +0200

    Refactorisation code page web

--- website/sco2-react/app/components/Menu3.jsx
+++ website/sco2-react/app/components/Menu3.jsx
@@ -1,10 +1,13 @@
-export default function Menu3(){
-    return(
+export default function Menu3() {
+    return (
         <div className="grid grid-rows-3 grid-flow-col gap-3 bg-[#F9EFDB] pr-4 pt-3 rounded-lg h-auto w-auto pb-5">
-        <div className="row-span-3"></div>
-            <div className="flex col-span-2 w-auto h-auto bg-white border-none rounded  items-center justify-center rounded-lg rounded-2xl inner-shadow"><p className="text-5xl font-sans">L’interface de SCO2</p></div>
+            <div className="row-span-3"></div>
+            <div className="flex col-span-2 w-auto h-auto bg-white border-none rounded  items-center justify-center rounded-lg rounded-2xl inner-shadow">
+                <p className="text-5xl font-sans">L&apos;interface de SCO2</p>
+            </div>
             <div className="row-span-2 col-span-2 w-auto h-auto bg-[#EBD9B4] border-none rounded-lg rounded-2xl items-center text-justify justify-center px-10 inner-shadow">
-                <p>Dotée d'une interface d'une remarquable efficacité, empreinte de minimalisme, SCO2 parvient à dédier ses ressources à l'aspect fondamental de son service : la facilitation des échanges entre les utilisateurs. Cette conception épurée, au-delà de conférer une expérience utilisateur des plus fluides et intuitives, incarne une approche où l'essence des interactions au sein de la plateforme est sublimée. En privilégiant la simplicité sans compromettre la fonctionnalité, SCO2 offre un cadre propice à des échanges fluides et enrichissants.</p></div>
-            </div> 
+                <p>Dotée d&apos;une interface d&apos;une remarquable efficacité, empreinte de minimalisme, SCO2 parvient à dédier ses ressources à l&apos;aspect fondamental de son service : la facilitation des échanges entre les utilisateurs. Cette conception épurée, au-delà de conférer une expérience utilisateur des plus fluides et intuitives, incarne une approche où l&apos;essence des interactions au sein de la plateforme est sublimée. En privilégiant la simplicité sans compromettre la fonctionnalité, SCO2 offre un cadre propice à des échanges fluides et enrichissants.</p>
+            </div>
+        </div>
     );
 }

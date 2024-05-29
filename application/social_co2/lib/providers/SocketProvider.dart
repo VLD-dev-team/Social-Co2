2ddc0feb83a81ac29df6613b3800da6f9761bdb9
Vrock691 <vrock691@gmail.com>
2024-05-29 16:06:48 +0200

    feat: Modification du handler du websocket

--- application/social_co2/lib/providers/SocketProvider.dart
+++ application/social_co2/lib/providers/SocketProvider.dart
@@ -1,28 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class SocketService with ChangeNotifier {
+class SocketProvider with ChangeNotifier {
+  // Définition du socket
   late IO.Socket _socket;
 
+  // Création des variables de connexion
   bool _isConnected = false;
   bool get isConnected => _isConnected;
 
-  SocketService() {
+  // Constructeur
+  SocketProvider() {
+    print('initialisation du socket');
     _initSocket();
   }
 
   void _initSocket() {
-    _socket = IO.io('https://api.social-co2.vld-group.com', <String, dynamic>{
-      'transports': ['websocket'],
-    });
+    final Map<String, dynamic> options = (kIsWeb)
+        ? {}
+        : {
+            'transports': ['websocket'], // Supporté uniquement sur mobile
+          };
+    _socket = IO.io('https://api.social-co2.vld-group.com', options);
+
+    _socket.onConnecting((data) => print(data));
+
+    _socket.onError((data) => print(data));
 
     _socket.on('connect', (_) {
       _isConnected = true;
+      print(_isConnected);
       notifyListeners();
     });
 
     _socket.on('disconnect', (_) {
       _isConnected = false;
+      print(_isConnected);
       notifyListeners();
     });
   }

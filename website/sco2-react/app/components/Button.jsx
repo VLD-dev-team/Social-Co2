5154bcc6cb615ccf70ab971f12536e307106da4d
Kez0X <118253031+Kez0X@users.noreply.github.com>
2025-03-03 07:03:18 +0000

    fix : link

--- website/sco2-react/app/components/Button.jsx
+++ website/sco2-react/app/components/Button.jsx
@@ -1,7 +1,7 @@
 'use client';
 export default function Button({children}){ 
     return(
-        <button className="bg-white text-green-600 px-6 py-2 rounded-full font-semibold hover:bg-green-50 transition duration-300 m-3" onClick={event => window.location.href='app.social-co2.vld-group.com'}>
+        <button className="bg-white text-green-600 px-6 py-2 rounded-full font-semibold hover:bg-green-50 transition duration-300 m-3" onClick={event => window.location.href='https://app.social-co2.vld-group.com/'}>
             {children}
         </button>
 
